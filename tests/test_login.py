0c47e54ecb8eafcb406dc8491671eee30696b422
dzastinka <jslawska5@gmail.com>
2023-12-15 14:46:07 +0100

    Add more files

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,15 @@
 import os
 
-from playwright.sync_api import Playwright, expect
+from playwright.sync_api import expect
 import pytest
 import re
 
+PASSWORD = os.environ['PASSWORD']
+
 
 @pytest.mark.smoke
-def test_login_page(set_up):
+def test_login_page(set_up, assert_snapshot):
     page = set_up
     expect(page).to_have_url(re.compile("google"))
+    assert_snapshot(page.screenshot(full_page=True))
     # page.fill("input filed selector", os.environ['PASSWORD'])
